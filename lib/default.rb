6af9fae310f7d709fe22c2069b090d91b4f734d4
Charles Hogg <charles.r.hogg@gmail.com>
2014-02-04 12:55:05 -0500

    Rules for processing blog posts

--- lib/default.rb
+++ lib/default.rb
@@ -3,3 +3,10 @@
 
 include Nanoc::Helpers::Blogging
 include Nanoc::Helpers::LinkTo
+
+# A 'short name' for a blog post, suitable for forming part of a URL.  It
+# doesn't have to be unique, but it should be unique per date.
+def ShortBlogName(item)
+  title = item[item.attributes.has_key?(:short_url) ? :short_url : :title]
+  title.split(%r{\W+}).join('_')
+end
