91963584983af52b314101b049dac2b9d4b94f05
Charles Hogg <charles.r.hogg@gmail.com>
2014-02-08 14:26:43 -0500

    Add :knitr filter

--- lib/default.rb
+++ lib/default.rb
@@ -10,3 +10,16 @@ def ShortBlogName(item)
   title = item[item.attributes.has_key?(:short_url) ? :short_url : :title]
   title.split(%r{\W+}).join('_')
 end
+
+
+class KnitrFilter < Nanoc::Filter
+  identifier :knitr
+
+  def run(content, params={})
+    # Escape content to fit inside R's double quotes inside bash single quotes.
+    escaped_content = content.gsub(%r{\\}, "\\\\\\").gsub(%r{"}, '\"').gsub(%r{'}, "'\"'\"'").gsub(%r{\n}, "\\n")
+    command = ('Rscript -e \'library(knitr); cat(knit(quiet=TRUE, text="' +
+               escaped_content + '"))\'')
+    `#{command}`
+  end
+end
