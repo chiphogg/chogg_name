e34d08c99de57a3ae70d4813f2cabc73be0e142e
Chip Hogg <charles.r.hogg@gmail.com>
2015-03-15 20:20:11 -0400

    Support figures in R snippets
    This involves factoring out the logic which sets output directories into
    a common helper.  That way, both the `Rules` and the helper functions
    can call this logic from a common source.
    
    The key insight is to set the base.dir option to the desired output
    directory.
    
    We also set the fig.path to empty, but this is just a matter of
    preference.

--- lib/default.rb
+++ lib/default.rb
@@ -11,15 +11,35 @@ def ShortBlogName(item)
   title.split(%r{\W+}).join('_')
 end
 
+# Custom logic for the output directory.
+#
+# We put it here so that other helpers can reference this logic too.
+def OutputDirectory(item)
+  if item.nil?
+    return ''
+  end
+  case item.identifier
+  when %r{/blog/.+}
+    root = item.attributes.has_key?(:created_at) ?
+      attribute_to_time(item[:created_at]).strftime('/blog/%Y/%m/%d') : '/draft'
+    return [root, ShortBlogName(item)].join('/')
+  else
+    return item.identifier
+  end
+end
 
 class KnitrFilter < Nanoc::Filter
   identifier :knitr
 
   def run(content, params={})
     # Escape content to fit inside R's double quotes inside bash single quotes.
+    output_dir = 'output' + OutputDirectory(@item)
     escaped_content = content.gsub(%r{\\}, "\\\\\\").gsub(%r{"}, '\"').gsub(%r{'}, "'\"'\"'").gsub(%r{\n}, "\\n")
-    command = ('Rscript -e \'library(knitr); cat(knit(quiet=TRUE, text="' +
-               escaped_content + '"))\'')
+    command = ('Rscript -e \'library(knitr);' +
+               'opts_knit$set(base.dir=normalizePath("' + output_dir + '"));' +
+               'opts_chunk$set(fig.path="");' +
+               'cat(knit(quiet=TRUE, output=NULL, input="' + escaped_content +
+               '"))\'')
     `#{command}`
   end
 end
